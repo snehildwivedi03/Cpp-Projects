a6848f485fbfaded316a38c18484585d049c56b0
snehildwivedi03 <snehildwivedi03@gmail.com>
2025-07-15 12:09:34 +0530

    Basic Folder Structure

---
+++ 2-BLOB-Mini-Shell/src/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <unistd.h>     // For getcwd()
+#include <limits.h>     // For PATH_MAX
+#include <cstdlib>      // For getenv()
+
+using namespace std;
+
+// Show welcome screen
+void showWelcome() {
+    cout << "\n";
+    cout << "🫧 ~ Welcome to blob-shell ~\n";
+    cout << "   Lightweight Unix Shell in C++\n";
+    cout << "   Type 'exit' to quit.\n";
+    cout << "--------------------------------\n\n";
+}
+
+// Show the prompt: blob:/current/path$
+void showPrompt() {
+    char cwd[PATH_MAX];
+    getcwd(cwd, sizeof(cwd));
+    cout << "blob:" << cwd << "$ ";
+}
+
+int main() {
+    showWelcome();
+
+    string input;
+    while (true) {
+        showPrompt();
+
+        getline(cin, input);
+
+        if (input.empty()) continue;
+
+        if (input == "exit") {
+            cout << "👋 Exiting blob-shell...\n";
+            break;
+        }
+
+        // Later we will call parser and executor here
+        cout << "[placeholder] You entered: " << input << "\n";
+    }
+
+    return 0;
+}
